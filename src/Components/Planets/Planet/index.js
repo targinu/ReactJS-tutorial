eb2494284a3fb469d35b4df750eb3295eea0f971
targinu <targino26@outlook.com>
2023-09-20 15:34:15 -0300

    Criando um component com props

--- src/Components/Planets/Planet/index.js
+++ src/Components/Planets/Planet/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GrayImg from "../../shared/gray_img";
+import DescriptionWithLink from "../../shared/descriptionWithLink";
 
 const Planet = (props) => {
   return (
     <div>
       <h4>{props.name}</h4>
       <p>
-        {props.description}
+        <DescriptionWithLink description={props.description} />
+        <DescriptionWithLink link={props.link} />
       </p>
       <GrayImg img_url={props.img_url} />
     </div>
--- src/Components/Planets/index.js
+++ src/Components/Planets/index.js
@@ -15,6 +15,7 @@ const Planets = () => {
         observado da Terra,        tendo uma magnitude aparente que varia de −2,6 a 5,7, embora não seja facilmente observado pois sua separação angular do Sol é de 
         apenas 28,3º. Uma vez que Mercúrio normalmente se perde no intenso brilho solar, exceto em eclipses solares, só pode ser observado a olho nu durante o crepúsculo 
         matutino ou vespertino."
+        link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
         img_url="https://media.istockphoto.com/id/183817880/pt/foto/mercury.jpg?s=1024x1024&w=is&k=20&c=Ie5migPD15F9REEdO7_Z7P_-cEIv7dZ3rUSPQE3zI6A="
       />
 
@@ -24,6 +25,7 @@ const Planets = () => {
         description="Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto 
         observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[11] 
         uma região de corpos além da órbita de Netuno."
+        link="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
         img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
       />
     </Fragment>
